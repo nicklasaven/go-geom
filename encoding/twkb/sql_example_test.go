9c83bb3071319f630adc34f10bb8cac4bfe627b7
Nicklas Avén <nicklas.aven@jordogskog.no>
2021-03-03 09:58:12 +0100

    initial twkb commit, not working but building

---
+++ encoding/twkb/sql_example_test.go
@@ -0,0 +1,172 @@
+package twkb_test
+/*
+import (
+	"fmt"
+	"log"
+
+	"github.com/DATA-DOG/go-sqlmock"
+
+	"github.com/twpayne/go-geom"
+	"github.com/twpayne/go-geom/encoding/wkb"
+	"github.com/twpayne/go-geom/internal/geomtest"
+)
+
+func Example_scan() {
+	type City struct {
+		Name     string
+		Location wkb.Point
+	}
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	mock.ExpectQuery(`SELECT name, ST_AsBinary\(location\) FROM cities WHERE name = \?;`).
+		WithArgs("London").
+		WillReturnRows(
+			sqlmock.NewRows([]string{"name", "location"}).
+				AddRow("London", geomtest.MustHexDecode("010100000052B81E85EB51C03F45F0BF95ECC04940")),
+		)
+
+	var c City
+	if err := db.QueryRow(`SELECT name, ST_AsBinary(location) FROM cities WHERE name = ?;`, "London").Scan(&c.Name, &c.Location); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Longitude: %v\n", c.Location.X())
+	fmt.Printf("Latitude: %v\n", c.Location.Y())
+
+	// Output:
+	// Longitude: 0.1275
+	// Latitude: 51.50722
+}
+
+func Example_value() {
+	type City struct {
+		Name     string
+		Location wkb.Point
+	}
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	mock.ExpectExec(`INSERT INTO cities \(name, location\) VALUES \(\?, \?\);`).
+		WithArgs("London", geomtest.MustHexDecode("010100000052B81E85EB51C03F45F0BF95ECC04940")).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	c := City{
+		Name:     "London",
+		Location: wkb.Point{Point: geom.NewPoint(geom.XY).MustSetCoords(geom.Coord{0.1275, 51.50722})},
+	}
+
+	result, err := db.Exec(`INSERT INTO cities (name, location) VALUES (?, ?);`, c.Name, &c.Location)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rowsAffected, _ := result.RowsAffected()
+	fmt.Printf("%d rows affected", rowsAffected)
+
+	// Output:
+	// 1 rows affected
+}
+
+func Example_scan_different_shapes() {
+	type Shape struct {
+		Name string
+		Geom wkb.Geom
+	}
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	mock.ExpectQuery(`SELECT name, ST_AsBinary\(geom\) FROM shapes`).
+		WillReturnRows(
+			sqlmock.NewRows([]string{"name", "location"}).
+				AddRow("Point", geomtest.MustHexDecode("0101000000000000000000F03F0000000000000040")).
+				AddRow("LineString", geomtest.MustHexDecode("010200000002000000000000000000F03F000000000000004000000000000008400000000000001040")).
+				AddRow("Polygon", geomtest.MustHexDecode("01030000000100000004000000000000000000F03F00000000000000400000000000000840000000000000104000000000000014400000000000001840000000000000F03F0000000000000040")),
+		)
+
+	rows, err := db.Query(`SELECT name, ST_AsBinary(geom) FROM shapes`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for rows.Next() {
+		var s Shape
+		err := rows.Scan(&s.Name, &s.Geom)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s: %v\n", s.Name, s.Geom.FlatCoords())
+	}
+
+	// Output:
+	// Point: [1 2]
+	// LineString: [1 2 3 4]
+	// Polygon: [1 2 3 4 5 6 1 2]
+}
+
+func Example_value_different_shapes() {
+	type Shape struct {
+		Name string
+		Geom wkb.Geom
+	}
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	mock.ExpectExec(`INSERT INTO objects \(name, location\) VALUES \(\?, \?\);`).
+		WithArgs("Point", geomtest.MustHexDecode("0101000000000000000000F03F0000000000000040")).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectExec(`INSERT INTO objects \(name, location\) VALUES \(\?, \?\);`).
+		WithArgs("LineString", geomtest.MustHexDecode("010200000002000000000000000000F03F000000000000004000000000000008400000000000001040")).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectExec(`INSERT INTO objects \(name, location\) VALUES \(\?, \?\);`).
+		WithArgs("Polygon", geomtest.MustHexDecode("01030000000100000004000000000000000000F03F00000000000000400000000000000840000000000000104000000000000014400000000000001840000000000000F03F0000000000000040")).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	shapes := []Shape{
+		{
+			Name: "Point",
+			Geom: wkb.Geom{T: geom.NewPoint(geom.XY).MustSetCoords(geom.Coord{1, 2})},
+		},
+		{
+			Name: "LineString",
+			Geom: wkb.Geom{T: geom.NewLineString(geom.XY).MustSetCoords([]geom.Coord{{1, 2}, {3, 4}})},
+		},
+		{
+			Name: "Polygon",
+			Geom: wkb.Geom{
+				T: geom.NewPolygon(geom.XY).MustSetCoords([][]geom.Coord{
+					{{1, 2}, {3, 4}, {5, 6}, {1, 2}},
+				}),
+			},
+		},
+	}
+
+	for _, s := range shapes {
+		result, err := db.Exec(`INSERT INTO objects (name, location) VALUES (?, ?);`, s.Name, &s.Geom)
+		if err != nil {
+			log.Fatal(err)
+		}
+		rowsAffected, _ := result.RowsAffected()
+		fmt.Printf("%d rows affected\n", rowsAffected)
+	}
+
+	// Output:
+	// 1 rows affected
+	// 1 rows affected
+	// 1 rows affected
+}
+*/
---
+++ encoding/twkb/twkb.go
@@ -0,0 +1,621 @@
+// Package wkb implements Tiny Well Known Binary encoding and decoding.
+//
+
+package twkb
+
+import (
+	"encoding/binary"
+//	"fmt"
+	"io"
+	"bytes"
+	"math"
+	"github.com/twpayne/go-geom"
+	"github.com/twpayne/go-geom/encoding/wkbcommon"
+)
+
+const (
+	wkbXYID   = 0
+	wkbXYZID  = 1000
+	wkbXYMID  = 2000
+	wkbXYZMID = 3000
+)
+type twkb struct{
+	nDims int
+	precision byte
+	lastCoords []int64
+	geomType byte
+	hasBbox bool
+	hasSize bool
+	hasId bool
+	isEmpty bool
+	hasZ bool
+	hasM bool
+	zPrecision byte
+	mPrecision byte
+	cursor *uint
+}
+
+// Read reads an arbitrary geometry from r.
+func Read(r io.ByteReader, opts ...wkbcommon.WKBOption) (geom.T, error) {
+
+
+	var t twkb 
+
+	t.readTWKBheader(r)
+
+	layout := geom.NoLayout
+
+	if !t.hasZ && !t.hasM && t.nDims == 2{		
+		layout = geom.XY
+	}else if t.hasZ && t.hasM && t.nDims == 4{
+		layout = geom.XYZM
+	}else if t.hasZ && t.nDims == 3{
+		layout = geom.XYZ		
+	}else if t.hasM && t.nDims == 3{
+		layout = geom.XYM		
+	}else {
+		return nil, wkbcommon.ErrUnknownType(t.geomType)
+	}
+
+
+	return t.ReadWoHeader(r ,layout, opts ...)
+}
+
+func (t twkb) ReadWoHeader(r io.ByteReader,layout geom.Layout ,  opts ...wkbcommon.WKBOption) (geom.T, error) {
+	params := wkbcommon.InitWKBParams(
+	wkbcommon.WKBParams{
+		EmptyPointHandling: wkbcommon.EmptyPointHandlingError,
+	},
+	opts...,
+	)
+	switch int(t.geomType) {
+	case wkbcommon.PointID:
+		flatCoords, err := t.ReadFlatCoords0(r)
+		if err != nil {
+			return nil, err
+		}
+		
+		
+		if params.EmptyPointHandling == wkbcommon.EmptyPointHandlingNaN {
+			return geom.NewPointFlatMaybeEmpty(layout, flatCoords), nil
+		}
+		return geom.NewPointFlat(layout, flatCoords), nil
+	case wkbcommon.LineStringID:
+		flatCoords, err := t.ReadFlatCoords1(r)
+		if err != nil {
+			return nil, err
+		}
+		return geom.NewLineStringFlat(layout, flatCoords), nil
+	case wkbcommon.PolygonID:
+		flatCoords, ends, err := t.ReadFlatCoords2(r)
+		if err != nil {
+			return nil, err
+		}
+		return geom.NewPolygonFlat(layout, flatCoords, ends), nil
+	case wkbcommon.MultiPointID:
+		n, err := readUvarint(r)
+		if err != nil {
+			return nil, err
+		}
+		if limit := wkbcommon.MaxGeometryElements[1]; limit >= 0 && int(n) > limit {
+			return nil, wkbcommon.ErrGeometryTooLarge{Level: 1, N: int(n), Limit: limit}
+		}
+		mp := geom.NewMultiPoint(layout)
+		for i := uint64(0); i < n; i++ {
+			g, err := t.ReadWoHeader(r,layout, opts...)
+			if err != nil {
+				return nil, err
+			}
+			p, ok := g.(*geom.Point)
+			if !ok {
+				return nil, wkbcommon.ErrUnexpectedType{Got: g, Want: &geom.Point{}}
+			}
+			if err = mp.Push(p); err != nil {
+				return nil, err
+			}
+		}
+		return mp, nil
+	case wkbcommon.MultiLineStringID:
+		n, err := readUvarint(r)
+		if err != nil {
+			return nil, err
+		}
+		if limit := wkbcommon.MaxGeometryElements[2]; limit >= 0 && int(n) > limit {
+			return nil, wkbcommon.ErrGeometryTooLarge{Level: 2, N: int(n), Limit: limit}
+		}
+		mls := geom.NewMultiLineString(layout)
+		for i := uint64(0); i < n; i++ {
+			g, err := t.ReadWoHeader(r,layout, opts...)
+			if err != nil {
+				return nil, err
+			}
+			p, ok := g.(*geom.LineString)
+			if !ok {
+				return nil, wkbcommon.ErrUnexpectedType{Got: g, Want: &geom.LineString{}}
+			}
+			if err = mls.Push(p); err != nil {
+				return nil, err
+			}
+		}
+		return mls, nil
+	case wkbcommon.MultiPolygonID:
+		n, err := readUvarint(r)
+		if err != nil {
+			return nil, err
+		}
+		if limit := wkbcommon.MaxGeometryElements[3]; limit >= 0 && int(n) > limit {
+			return nil, wkbcommon.ErrGeometryTooLarge{Level: 3, N: int(n), Limit: limit}
+		}
+		mp := geom.NewMultiPolygon(layout)
+		for i := uint64(0); i < n; i++ {
+			g, err := t.ReadWoHeader(r,layout, opts...)
+			if err != nil {
+				return nil, err
+			}
+			p, ok := g.(*geom.Polygon)
+			if !ok {
+				return nil, wkbcommon.ErrUnexpectedType{Got: g, Want: &geom.Polygon{}}
+			}
+			if err = mp.Push(p); err != nil {
+				return nil, err
+			}
+		}
+		return mp, nil
+	case wkbcommon.GeometryCollectionID:
+		n, err := readUvarint(r)
+		if err != nil {
+			return nil, err
+		}
+		gc := geom.NewGeometryCollection()
+		for i := uint64(0); i < n; i++ {
+			g, err := Read(r, opts...)
+			if err != nil {
+				return nil, err
+			}
+			if err := gc.Push(g); err != nil {
+				return nil, err
+			}
+		}
+		return gc, nil
+	default:
+		return nil, wkbcommon.ErrUnsupportedType(uint32(t.geomType))
+	}
+}
+
+func (t *twkb)readTWKBheader(r io.ByteReader)(error){
+	
+
+	byte1, err := r.ReadByte()
+	if err != nil {
+		return  err
+	}
+	
+	t.geomType = byte(byte1 & 15)
+
+	t.precision = byte1>>4
+	extended := false
+	byte2, err := r.ReadByte()
+	if err != nil {
+		return nil
+	}
+	
+	if byte2 & 1 > 0{
+		t.hasBbox = true
+	}
+	if byte2 & 2 > 0{
+		t.hasSize = true
+	}
+	if byte2 & 4 > 0{
+		t.hasId = true
+	}
+	if byte2 & 8 > 0{
+		extended = true
+	}
+	if byte2 & 16 > 0{
+		t.isEmpty = true
+	}
+	
+	t.nDims= 2
+	if(extended){
+		byte3, err := r.ReadByte()
+		if err != nil {
+			return nil
+		}
+		if byte3 & 1 > 0{
+			t.hasZ = true
+			t.nDims++
+		}
+		if byte3 & 2 > 0{
+			t.hasM = true
+			t.nDims++
+		}
+		if t.hasZ{
+			t.zPrecision = (byte3 & 28) >> 2
+		}
+		if t.hasM{
+			t.mPrecision = (byte3 & 224) >> 5
+		}
+	}	
+	return nil
+}
+
+type resbuf struct{
+data []byte
+}
+// Unmarshal unmrshals an arbitrary geometry from a []byte.
+func Unmarshal(data []byte, opts ...wkbcommon.WKBOption) (geom.T, error) {
+	return Read(bytes.NewBuffer(data), opts...)
+}
+
+// Write writes an arbitrary geometry to w.
+func Write(buf resbuf,precisionXY byte,precisionZ byte, precisionM byte, g geom.T) error {
+
+
+
+	if g.Empty(){
+		buf.resize(2)
+		buf.data[0] = 0
+		buf.data[1] = 16
+		return nil
+	}
+
+var err error
+/*
+	params := wkbcommon.InitWKBParams(
+		wkbcommon.WKBParams{
+			EmptyPointHandling: wkbcommon.EmptyPointHandlingError,
+		},
+		opts...,
+	)
+	*/
+
+	var t twkb
+
+	switch g.(type) {
+	case *geom.Point:
+		t.geomType = byte(wkbcommon.PointID)
+	case *geom.LineString:
+		t.geomType = byte(wkbcommon.LineStringID)
+	case *geom.Polygon:
+		t.geomType = byte(wkbcommon.PolygonID)
+	case *geom.MultiPoint:
+		t.geomType = byte(wkbcommon.MultiPointID)
+	case *geom.MultiLineString:
+		t.geomType = byte(wkbcommon.MultiLineStringID)
+	case *geom.MultiPolygon:
+		t.geomType = byte(wkbcommon.MultiPolygonID)
+	case *geom.GeometryCollection:
+		t.geomType = byte(wkbcommon.GeometryCollectionID)
+	default:
+		return geom.ErrUnsupportedType{Value: g}
+	}
+
+
+	t.nDims = g.Stride()
+	switch g.Layout() {
+	case geom.NoLayout:
+		// Special case for empty GeometryCollections
+		if _, ok := g.(*geom.GeometryCollection); !ok || !g.Empty() {
+			return geom.ErrUnsupportedLayout(g.Layout())
+		}
+	case geom.XY:
+		t.hasZ = false
+		t.hasM = false
+	case geom.XYZ:
+		t.hasZ = true
+		t.zPrecision = precisionZ
+		t.hasM = false
+	case geom.XYM:
+		t.hasZ = false
+		t.hasM = true
+		t.mPrecision = precisionM
+	case geom.XYZM:
+		t.hasZ = true
+		t.zPrecision = precisionZ
+		t.hasM = true
+		t.mPrecision = precisionM
+	default:
+		return geom.ErrUnsupportedLayout(g.Layout())
+	}
+
+
+	err = t.writeHeader(buf)
+	if err != nil{
+		return err
+	}
+
+	t.writeWoHeader(buf, g)
+	return nil
+}
+
+func (t twkb) writeWoHeader(buf resbuf,g geom.T)(error){
+
+	switch g := g.(type) {
+	case *geom.Point:
+		return t.writeFlatCoords0(buf, g.FlatCoords())
+	case *geom.LineString:
+		return t.writeFlatCoords1(buf, g.FlatCoords())
+	case *geom.Polygon:
+		return t.writeFlatCoords2(buf, g.FlatCoords(), g.Ends())
+	case *geom.MultiPoint:
+		n := g.NumPoints()
+		buf.appendUvarint(uint64(n))
+		for i := 0; i < n; i++ {
+			if err := t.writeWoHeader(buf,g.Point(i)); err != nil {
+				return err
+			}
+		}
+		return nil
+	case *geom.MultiLineString:
+		n := g.NumLineStrings()
+		buf.appendUvarint(uint64(n))
+		for i := 0; i < n; i++ {
+			if err := t.writeWoHeader(buf, g.LineString(i)); err != nil {
+				return err
+			}
+		}
+		return nil
+	case *geom.MultiPolygon:
+		n := g.NumPolygons()		
+		buf.appendUvarint(uint64(n))
+		for i := 0; i < n; i++ {
+			if err := t.writeWoHeader(buf, g.Polygon(i)); err != nil {
+				return err
+			}
+		}
+		return nil
+	case *geom.GeometryCollection:
+		n := g.NumGeoms()		
+		buf.appendUvarint(uint64(n))
+		for i := 0; i < n; i++ {
+			if err := Write(buf,t.precision, t.zPrecision, t.mPrecision, g.Geom(i)); err != nil {
+				return err
+			}
+		}
+		return nil
+	default:
+		return geom.ErrUnsupportedType{Value: g}
+	}
+}
+
+// Marshal marshals an arbitrary geometry to a []byte.
+func Marshal(g geom.T,precisionXY byte, precisionZ byte, precisionM byte, opts ...wkbcommon.WKBOption) ([]byte, error) {
+	var res []byte
+	
+	if err := Write(resbuf{data:res}, precisionXY, precisionZ, precisionM, g); err != nil {
+		return nil, err
+	}
+	return []byte(res), nil
+}
+
+
+func (t twkb) writeHeader(buf resbuf)(error){
+	
+	buf.resize(3)
+	
+	var firstByte byte 
+	firstByte &= byte(t.precision)
+	firstByte = firstByte<< 4	
+	firstByte |= byte(t.geomType)
+
+	buf.data[0] = firstByte
+
+	/*TODO add support for bbox, size id-list and empty*/
+	var secondByte byte = 0
+
+
+	var thirdByte byte = 0
+	if t.hasZ || t.hasM{
+		secondByte &= 8 // we set the fourth bit indicating extended precision information byte
+		var zbyte, mbyte byte
+		if t.hasZ{
+			zbyte := t.zPrecision << 2
+			zbyte |= 1
+		}
+		if t.hasM{
+			mbyte := t.mPrecision << 5
+			mbyte |= 2			
+		}
+		thirdByte = zbyte | mbyte
+	}
+
+	
+	buf.data[1] = secondByte
+	if thirdByte > 0{
+		buf.data[2] = thirdByte	
+	}
+	
+	return nil	
+}
+
+
+
+// ReadFlatCoords0 reads flat coordinates 0.
+func (t twkb) ReadFlatCoords0(r io.ByteReader) ([]float64, error) {
+	coord, err := t.readFloatArray(r, 1)
+	if err != nil {
+		return nil, err
+	}
+	return coord, nil
+}
+
+// ReadFlatCoords1 reads flat coordinates 1.
+func  (t twkb) ReadFlatCoords1(r io.ByteReader) ([]float64, error) {
+	n, err := readUvarint(r)
+	if err != nil {
+		return nil, err
+	}
+	if limit := wkbcommon.MaxGeometryElements[1]; limit >= 0 && int(n) > limit {
+		return nil, wkbcommon.ErrGeometryTooLarge{Level: 1, N: int(n), Limit: limit}
+	}
+
+	flatCoords, err := t.readFloatArray(r, n)
+	if err != nil{
+		return nil, err
+	}
+	return flatCoords, nil
+}
+
+// ReadFlatCoords2 reads flat coordinates 2.
+func (t twkb) ReadFlatCoords2(r io.ByteReader) ([]float64, []int, error) {
+	n, err := readUvarint(r)
+	if err != nil {
+		return nil, nil, err
+	}
+	if limit := wkbcommon.MaxGeometryElements[2]; limit >= 0 && int(n) > limit {
+		return nil, nil, wkbcommon.ErrGeometryTooLarge{Level: 2, N: int(n), Limit: limit}
+	}
+	var flatCoordss []float64
+	var ends []int
+	for i := 0; i < int(n); i++ {
+		flatCoords, err := t.ReadFlatCoords1(r)
+		if err != nil {
+			return nil, nil, err
+		}
+		flatCoordss = append(flatCoordss, flatCoords...)
+		ends = append(ends, len(flatCoordss))
+	}
+	return flatCoordss, ends, nil
+}
+
+func (t twkb) readFloatArray(r io.ByteReader, n uint64)([]float64, error){
+	
+	flatCoords := make([]float64, int(n)*t.nDims)
+	for i:=0; i<int(n); i++{	
+		for j:=0; j<t.nDims; j++{
+			v, err := binary.ReadVarint(r)
+			if err != nil{return nil, err}
+			t.lastCoords[j] += v
+			flatCoords[i*t.nDims + j] = float64(t.lastCoords[j])/math.Pow(10.0,float64(t.precision))
+		}
+	}
+	return flatCoords, nil
+}
+
+
+
+func (t twkb) readVarint2float64(r io.ByteReader)(float64, error){
+	intVal, err := binary.ReadVarint(r)
+	if err != nil{
+		return 0, err 
+	}
+	return float64(intVal)/math.Pow(10.0,float64(t.precision))	, nil
+}
+
+
+
+
+func readUvarint(r io.ByteReader)(uint64, error){
+	
+	uIntVal, err := binary.ReadUvarint(r)	
+	if err != nil{
+		return 0, err 
+	}
+	return uIntVal, nil
+}
+
+func readVarint(r io.ByteReader)(int64, error){
+	
+	intVal, err := binary.ReadVarint(r)	
+	if err != nil{
+		return 0, err 
+	}
+	return intVal, nil
+}
+
+
+
+
+
+
+// WriteFlatCoords0 writes flat coordinates 0.
+func (t twkb) writeFlatCoords0(buf resbuf, coords []float64) error {
+		t.writeFloatArray(buf, coords)
+	return nil
+}
+
+// WriteFlatCoords1 writes flat coordinates 1.
+func (t twkb) writeFlatCoords1(buf resbuf, coords []float64) error {	
+	t.writeFloatArray(buf, coords)
+	return nil}
+
+// WriteFlatCoords2 writes flat coordinates 2.
+func (t twkb) writeFlatCoords2(buf resbuf,  flatCoords []float64, ends []int) error {
+	buf.appendUvarint(uint64(len(ends)))
+	offset := 0
+	for _, end := range ends {
+		if err := t.writeFlatCoords1(buf, flatCoords[offset:end]); err != nil {
+			return err
+		}
+		offset = end
+	}
+	return nil
+}
+
+
+
+
+
+func (t twkb)writeFloatArray(buf resbuf, coords []float64){
+
+	nPoints := uint64(len(coords)/t.nDims)
+	buf.appendUvarint(uint64(nPoints))
+
+	nDims := t.nDims
+	for i:=uint64(0);i<nPoints;i++{
+		for j:=0;j<nDims;j++{
+				newVal := int64(math.Round(coords[int(i)*nDims + j] * (float64(math.Pow(10.0,float64(t.precision))))))
+				nextDelta := newVal - t.lastCoords[j]				
+				buf.appendVarint(nextDelta)
+				t.lastCoords[j] += nextDelta			
+		}
+	}
+}
+
+
+func (s resbuf)appendVarint(val int64){	
+	buf := make([]byte, binary.MaxVarintLen64)	
+	n := binary.PutVarint(buf, val)	
+	
+	m := s.resize(n)
+	copy(s.data[m:m+n], buf)
+//	*dst = append(*dst, buf[0:n] ...)
+}
+
+func (s resbuf)appendUvarint(val uint64){	
+	buf := make([]byte, binary.MaxVarintLen64)	
+	n := binary.PutUvarint(buf, val)	
+	m := s.resize(n)	
+	copy(s.data[m:m+n], buf)
+	
+	
+//	*dst = append(*dst, buf[0:n] ...)
+}
+/*
+func appendUVarint(dst *[]byte, val uint64){	
+	buf := make([]byte, binary.MaxVarintLen64)	
+	n := binary.PutUvarint(buf, val)	
+	*dst = append(*dst, buf[0:n] ...)
+}
+*/
+
+func (s resbuf)resize(n int)(int){
+	var res []byte
+	m := len(s.data)
+	if cap(s.data) < m + n {
+		var newSize int
+		if cap(s.data) == 0{
+			newSize = 16
+		}
+		for newSize < m+n{
+			newSize *= 2
+		}
+
+		res = make([]byte,m+n, newSize)
+		copy(res, s.data[:m])
+		s.data = res
+		return m
+	}
+	s.data = s.data[0:m+n]
+	return m
+}
